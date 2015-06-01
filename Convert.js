a60e46d2d5d9e7e19cba43724152b418cff1bcec
s14007 <s14007@std.it-college.ac.jp>
2015-06-01 15:32:26 +0900

    add

--- Convert.js
+++ Convert.js
@@ -1,14 +1,22 @@
 var dif = 1988;
 
 function convertCalender(year) {
+    if (year > dif) {
+      return convertSeirekiToWareki(year);
+    } else {
+      return convertWarekiToSeireki(year);
+    }
+}
 
-}
 function convertWarekiToSeireki(year) {
-  return year + dif;
+    return year + dif;
 }
+
 function convertSeirekiToWareki(year) {
-  return year - dif;
+    return year - dif;
 }
 
 console.log("西暦" + convertWarekiToSeireki(27) + "年");
 console.log("平成" + convertSeirekiToWareki(2015) + "年");
+console.log("西暦" + convertCalender(27) + "年");
+console.log("平成" + convertCalender(2015) + "年");
